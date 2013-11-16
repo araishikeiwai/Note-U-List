5b60745496915304d3fbe15f1b603ff52a252f7c
Umar Abdul Aziz <umaraziz.92@gmail.com>
2013-11-16 16:58:44 +0700

    layout (2)
    lanjutan tadi

--- gen/tekmob/nfc/note_u_list/R.java
+++ gen/tekmob/nfc/note_u_list/R.java
@@ -25,36 +25,34 @@ public final class R {
         public static final int ic_stat_name=0x7f020001;
     }
     public static final class id {
-        public static final int CheckBox01=0x7f08000b;
-        public static final int CheckBox02=0x7f08000a;
-        public static final int CheckBox03=0x7f080009;
-        public static final int CheckBox04=0x7f080006;
-        public static final int CheckBox05=0x7f080007;
+        public static final int CheckBox01=0x7f080012;
+        public static final int CheckBox02=0x7f08000d;
+        public static final int CheckBox03=0x7f080011;
+        public static final int CheckBox04=0x7f08000e;
+        public static final int CheckBox05=0x7f08000f;
         public static final int ImageButton01=0x7f080004;
         public static final int ImageButton02=0x7f080003;
         public static final int ImageButton03=0x7f080002;
         public static final int ImageButton04=0x7f080005;
-        public static final int ImageView01=0x7f080010;
-        public static final int ImageView02=0x7f080011;
-        public static final int ImageView03=0x7f080012;
-        public static final int ImageView04=0x7f080013;
-        public static final int action_settings=0x7f080016;
-        public static final int checkBox1=0x7f080008;
-        public static final int editText1=0x7f08000d;
+        public static final int ImageView01=0x7f080006;
+        public static final int ImageView02=0x7f080007;
+        public static final int ImageView03=0x7f080009;
+        public static final int ImageView04=0x7f080008;
+        public static final int action_settings=0x7f080014;
+        public static final int checkBox1=0x7f080010;
+        public static final int editText1=0x7f08000c;
         public static final int imageButton1=0x7f080000;
-        public static final int imageButton2=0x7f08000e;
-        public static final int imageButton3=0x7f08000f;
+        public static final int imageButton2=0x7f080013;
         public static final int imageView1=0x7f080001;
-        public static final int imageView2=0x7f080014;
-        public static final int searchView1=0x7f08000c;
-        public static final int seekBar1=0x7f080015;
+        public static final int imageView2=0x7f08000a;
+        public static final int searchView1=0x7f08000b;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int home=0x7f030001;
         public static final int kamera=0x7f030002;
-        public static final int list=0x7f030003;
-        public static final int notes=0x7f030004;
+        public static final int listfile=0x7f030003;
+        public static final int menu=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
