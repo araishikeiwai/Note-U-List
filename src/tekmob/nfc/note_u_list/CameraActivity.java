b7d82c749b6e76f88406a8082c4a56b814bcd1b0
araishikeiwai <rick@araishikeiwai.com>
2013-11-16 23:09:16 +0700

    Add activities, pre-branching

--- src/tekmob/nfc/note_u_list/CameraActivity.java
+++ src/tekmob/nfc/note_u_list/CameraActivity.java
@@ -1,17 +1,22 @@
 package tekmob.nfc.note_u_list;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
 
 public class CameraActivity extends Activity {
 
-	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.kamera);
-	
-	    // TODO Auto-generated method stub
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_camera);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.camera, menu);
+		return true;
 	}
 
 }
--- src/tekmob/nfc/note_u_list/ViewNoteActivity.java
+++ src/tekmob/nfc/note_u_list/ViewNoteActivity.java
@@ -4,18 +4,18 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class NoteActivity extends Activity {
+public class ViewNoteActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_note);
+		setContentView(R.layout.activity_view_note);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.note, menu);
+		getMenuInflater().inflate(R.menu.view_note, menu);
 		return true;
 	}
 
