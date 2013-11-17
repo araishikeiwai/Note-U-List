5b10e25f45d872821f761de6f4ba80e87b4c76ea
araishikeiwai <rick@araishikeiwai.com>
2013-11-18 05:23:37 +0700

    picture save (title/tag) ok, result activity ok

--- src/tekmob/nfc/note_u_list/CameraActivity.java
+++ src/tekmob/nfc/note_u_list/CameraActivity.java
@@ -8,6 +8,12 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.net.Uri;
@@ -16,41 +22,24 @@ import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class CameraActivity extends Activity {
+public class CameraActivity extends Activity implements PictureCallback {
 
 	public static final int MEDIA_TYPE_IMAGE = 1;
 	public static final int MEDIA_TYPE_VIDEO = 2;
 	protected static final String TAG = "Note-U-List! Camera";
 	private Camera mCamera;
 	private CameraPreview mPreview;
+	private byte[] mPictureData;
 	private ImageButton mCaptureButton;
-	private PictureCallback mPicture = new PictureCallback() {
-
-		@Override
-		public void onPictureTaken(byte[] data, Camera camera) {
-			File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
-			if (pictureFile == null) {
-				Log.d(TAG,
-						"Error creating media file, check storage permissions");
-				return;
-			}
-
-			try {
-				FileOutputStream fos = new FileOutputStream(pictureFile);
-				fos.write(data);
-				fos.close();
-				Log.d(TAG, "FILE SAVED!");
-			} catch (FileNotFoundException e) {
-				Log.d(TAG, "File not found: " + e.getMessage());
-			} catch (IOException e) {
-				Log.d(TAG, "Error accessing file: " + e.getMessage());
-			}
-		}
-	};
+	private Activity mActivity = this;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -67,7 +56,7 @@ public class CameraActivity extends Activity {
 		mCaptureButton = (ImageButton) findViewById(R.id.button_capture);
 		mCaptureButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				mCamera.takePicture(null, null, mPicture);
+				mCamera.takePicture(null, null, CameraActivity.this);
 			}
 		});
 	}
@@ -105,45 +94,131 @@ public class CameraActivity extends Activity {
 		return c;
 	}
 
-	private static Uri getOutputMediaFileUri(int type) {
-		return Uri.fromFile(getOutputMediaFile(type));
+	@Override
+	public void onPictureTaken(byte[] data, Camera camera) {
+		((FrameLayout) findViewById(R.id.camera_preview)).removeAllViews();
+		mCamera.stopPreview();
+		mPictureData = data;
+		setContentView(R.layout.activity_camera_captured);
+
+		// get the image and rotate it to display
+		final Bitmap image = BitmapFactory
+				.decodeByteArray(data, 0, data.length);
+		Matrix matrix = new Matrix();
+		matrix.postRotate(90);
+		Bitmap scaledBitmap = Bitmap.createScaledBitmap(image,
+				image.getWidth(), image.getHeight(), true);
+		Bitmap rotatedBitmap = Bitmap
+				.createBitmap(scaledBitmap, 0, 0, scaledBitmap.getWidth(),
+						scaledBitmap.getHeight(), matrix, true);
+		ImageView imageView = (ImageView) findViewById(R.id.camera_result);
+		imageView.setImageBitmap(rotatedBitmap);
+
+		ImageButton saveButton, discardButton, shareButton;
+		saveButton = (ImageButton) findViewById(R.id.button_camera_captured_save);
+		discardButton = (ImageButton) findViewById(R.id.button_camera_captured_discard);
+		shareButton = (ImageButton) findViewById(R.id.button_camera_captured_share);
+
+		saveButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent result = new Intent(mActivity.getApplicationContext(),
+						ResultActivity.class);
+				startActivityForResult(result, ResultActivity.GET_TITLE_TAG);
+			}
+		});
+		
+		discardButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				// TODO when migrating to API 4.0 and above, change to recreate() method!
+				Toast.makeText(mActivity, "Picture note discarded!", Toast.LENGTH_SHORT).show();
+				Intent intent = getIntent();
+				finish();
+				startActivity(intent);
+			}
+		});
+		
+		shareButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				// TODO implement!
+				Toast.makeText(mActivity, "NOT YET IMPLEMENTED", Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 
-	/** Create a File for saving an image or video */
-	private static File getOutputMediaFile(int type) {
-		// To be safe, you should check that the SDCard is mounted
-		// using Environment.getExternalStorageState() before doing this.
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		Log.d(TAG, "In onActivityResult()");
+		if (data != null) {
+			if (requestCode == ResultActivity.GET_TITLE_TAG
+					&& resultCode == RESULT_OK) {
+				Log.d(TAG,
+						"YEAAYY SAVED::"
+								+ data.getExtras().get(
+										ResultActivity.NOTE_TITLE) + "::"
+								+ data.getExtras().get(ResultActivity.NOTE_TAG));
 
+				File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE,
+						(String) data.getExtras()
+								.get(ResultActivity.NOTE_TITLE));
+				if (pictureFile == null) {
+					Log.d(TAG,
+							"Error creating media file, check storage permissions");
+					return;
+				}
+
+				// TODO organize tags into database
+				// TODO put link into database
+
+				try {
+					FileOutputStream fos = new FileOutputStream(pictureFile);
+					fos.write(mPictureData);
+					fos.close();
+					Log.d(TAG, "FILE SAVED!");
+					Toast.makeText(mActivity, "Picture note saved!", Toast.LENGTH_SHORT).show();
+				} catch (FileNotFoundException e) {
+					Log.d(TAG, "File not found: " + e.getMessage());
+				} catch (IOException e) {
+					Log.d(TAG, "Error accessing file: " + e.getMessage());
+				}
+				
+				finish();
+			}
+		}
+	}
+
+	private static File getOutputMediaFile(int type, String noteTitle) {
 		File mediaStorageDir = new File(
 				Environment
 						.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
 				"Note-U-List!");
-		Log.d(TAG, mediaStorageDir.getPath());
-		// This location works best if you want the created images to be shared
-		// between applications and persist after your app has been uninstalled.
-
+		
 		// Create the storage directory if it does not exist
 		if (!mediaStorageDir.exists()) {
 			if (!mediaStorageDir.mkdirs()) {
-				Log.d("Note-U-List!", "failed to create directory");
+				Log.d(TAG, "failed to create directory");
 				return null;
 			}
 		}
 
 		// Create a media file name
-		String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss")
-				.format(new Date());
+		String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
 		File mediaFile;
 		if (type == MEDIA_TYPE_IMAGE) {
 			mediaFile = new File(mediaStorageDir.getPath() + File.separator
-					+ timeStamp + ".jpg");
-		} else if (type == MEDIA_TYPE_VIDEO) {
-			mediaFile = new File(mediaStorageDir.getPath() + File.separator
-					+ timeStamp + ".mp4");
+					+ timeStamp + "_" + noteTitle + ".jpg");
 		} else {
 			return null;
 		}
 
+		// if duplicate, append "_" at the end of the filename
+		while (mediaFile.exists()) {
+			mediaFile = new File(mediaFile.getPath().substring(0,
+					mediaFile.getPath().length() - 4)
+					+ "_.jpg");
+		}
+
+		Log.d(TAG, mediaFile.getPath());
 		return mediaFile;
 	}
 
---
+++ src/tekmob/nfc/note_u_list/ResultActivity.java
@@ -0,0 +1,82 @@
+package tekmob.nfc.note_u_list;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.Toast;
+
+public class ResultActivity extends Activity {
+
+	public static final String NOTE_TITLE = "note title";
+	public static final String NOTE_TAG = "note_tag";
+	public static final int GET_TITLE_TAG = 21;
+	private static final String TAG = "Note-U-List! Result";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_result);
+
+		final EditText noteTitle = (EditText) findViewById(R.id.note_title);
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_dropdown_item_1line,
+				getTagsAutocompletes());
+		final MultiAutoCompleteTextView noteTag = (MultiAutoCompleteTextView) findViewById(R.id.note_tag);
+		MultiAutoCompleteTextView.CommaTokenizer tokenizer = new MultiAutoCompleteTextView.CommaTokenizer();
+
+		noteTag.setAdapter(adapter);
+		noteTag.setTokenizer(tokenizer);
+
+		Button submit_button = (Button) findViewById(R.id.button_submit);
+		submit_button.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if (noteTitle.getText().toString().equals("")) {
+					Log.d(TAG, "Content invalid");
+					Toast t = Toast.makeText(ResultActivity.this,
+							"Insert title!", Toast.LENGTH_SHORT);
+					t.show();
+				} else {
+					Log.d(TAG, "Content valid");
+					Intent intent = new Intent();
+					intent.putExtra(NOTE_TITLE, noteTitle.getText().toString());
+					intent.putExtra(NOTE_TAG, noteTag.getText().toString());
+					Log.d(TAG,
+							"FINISH RESULT::"
+									+ intent.getExtras().get(
+											ResultActivity.NOTE_TITLE)
+									+ "::"
+									+ intent.getExtras().get(
+											ResultActivity.NOTE_TAG));
+					setResult(RESULT_OK, intent);
+					finish();
+				}
+			}
+		});
+	}
+
+	private String[] getTagsAutocompletes() {
+		// TODO get from database
+		return new String[] { "aDum", "bDum", "cDum", "dDum", "eDum", "fDum",
+				"gDum", "hDum", "iDum", "jDum", "kDum", "lDum", "mDum", "nDum",
+				"oDum", "pDum", "qDum", "rDum", "sDum", "tDum", "uDum", "vDum",
+				"wDum", "xDum", "yDum", "zDum" };
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.result, menu);
+		return true;
+	}
+
+}
