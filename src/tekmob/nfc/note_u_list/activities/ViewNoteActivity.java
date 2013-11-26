213dca5f620a29be00c99a1366eb96661e4435a9
Umar <umarziz.92@gmail.com>
2013-11-26 10:16:39 +0700

    delete rename done!

--- src/tekmob/nfc/note_u_list/activities/ViewNoteActivity.java
+++ src/tekmob/nfc/note_u_list/activities/ViewNoteActivity.java
@@ -6,10 +6,15 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+
+
 import tekmob.nfc.note_u_list.R;
 import tekmob.nfc.note_u_list.helpers.DBAdapter;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
+
+import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
@@ -24,6 +29,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -34,6 +40,7 @@ import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
@@ -44,7 +51,11 @@ public class ViewNoteActivity extends Activity {
 	private static final int MENU_OPEN = Menu.FIRST + 4;
 	private static final int MENU_DELETE = Menu.FIRST + 5;
 	private static final int MENU_RENAME = Menu.FIRST + 6;
+	private static final int DIALOG_DELETE = 2;
+	private static final int DIALOG_RENAME = 3;
 	private File mContextFile = new File("");
+	private String mContextText = "";
+	private int id = 0;
 	List<String> filename;
 	List<String> mime;
 	Cursor c;
@@ -52,6 +63,10 @@ public class ViewNoteActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_view_note);
+		refresh();
+		
+	}
+	public void refresh(){
 		listView = (ListView) findViewById(R.id.list);
 		registerForContextMenu(listView);
 		DBAdapter db = new DBAdapter(ViewNoteActivity.this);
@@ -93,16 +108,20 @@ public class ViewNoteActivity extends Activity {
 	@Override
 	public boolean onContextItemSelected(MenuItem item) {
 		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		DBAdapter db = new DBAdapter(this);
+		int count = db.getBerkasCount();
 		int name = info.position;
+		id = count - name;
+		mContextText = filename.get(name);
 		switch (item.getItemId()) {
 		case MENU_OPEN:
 			openFiles(filename.get(name),mime.get(name));
 			return true;
 		case MENU_DELETE:
-			Toast.makeText(this, "function 1 called", Toast.LENGTH_SHORT).show(); 
+			showDialog(DIALOG_DELETE);
 			return true;
 		case MENU_RENAME:
-			Toast.makeText(this, "function 2 called", Toast.LENGTH_SHORT).show();  
+			showDialog(DIALOG_RENAME);
 			return true;
 		default:
 			return super.onContextItemSelected(item);
@@ -118,6 +137,102 @@ public class ViewNoteActivity extends Activity {
 		} catch (ActivityNotFoundException e) {
 			Toast.makeText(this, R.string.application_not_available, Toast.LENGTH_SHORT).show();
 		};
+	}
+private void deleteFileOrFolder(File file) {
+		DBAdapter db = new DBAdapter(this);
+		
+		if (file.delete()) {
+			db.open();
+			db.deleteBerkas(file.getName());
+			db.close();
+			// Delete was successful.
+			Toast.makeText(this, R.string.file_deleted, Toast.LENGTH_SHORT).show();
+			
+		} else {
+			Toast.makeText(this, R.string.error_deleting_file, Toast.LENGTH_SHORT).show();
+			
+		}
+		refresh();
+	}
+	
+	private void renameFileOrFolder(File file, String newFileName) {
+		File newFile = new File(newFileName);
+		rename(file, newFile);
+		DBAdapter db = new DBAdapter(this);
+		db.open();
+		db.updateBerkas(newFile.getName(), newFile.getAbsolutePath(), file.getName());
+		refresh();
+		
+	}
+
+	/**
+	 * @param oldFile
+	 * @param newFile
+	 */
+	private void rename(File oldFile, File newFile) {
+		int toast = 0;
+		if (oldFile.renameTo(newFile)) {
+			// Rename was successful.
+				toast = R.string.file_renamed;
+		} else {
+				toast = R.string.error_renaming_file;
+			
+		}
+		Toast.makeText(this, toast, Toast.LENGTH_SHORT).show();
+	}
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		final File file = new File(mContextText);
+		switch (id) {
+		case DIALOG_DELETE:
+			AlertDialog alert_back = new AlertDialog.Builder(this).create();
+			alert_back.setTitle(getString(R.string.really_delete, mContextText));
+
+			alert_back.setButton("No", new DialogInterface.OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();
+				}
+			});
+
+			alert_back.setButton2("Yes", new DialogInterface.OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					deleteFileOrFolder(file);
+				}
+			});
+			alert_back.show();
+			break;	
+		case DIALOG_RENAME:
+			LayoutInflater inflater = LayoutInflater.from(this);
+			View view = inflater.inflate(R.layout.dialog_new_folder, null);
+			final EditText et2 = (EditText) view
+				.findViewById(R.id.foldername);
+			et2.setText(mContextText);
+			AlertDialog alert_bac = new AlertDialog.Builder(this).create();
+			alert_bac.setTitle(getString(R.string.menu_rename));
+			alert_bac.setView(view);
+			alert_bac.setButton("No", new DialogInterface.OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();
+				}
+			});
+
+			alert_bac.setButton2("Yes", new DialogInterface.OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					renameFileOrFolder(file, et2.getText().toString());
+				}
+			});
+			alert_bac.show();
+			break;
+		}
+		return null;
 	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
--- src/tekmob/nfc/note_u_list/helpers/DBAdapter.java
+++ src/tekmob/nfc/note_u_list/helpers/DBAdapter.java
@@ -1,5 +1,6 @@
 package tekmob.nfc.note_u_list.helpers;
 
+
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
@@ -12,17 +13,19 @@ public class DBAdapter {
 	private static final String TAG="DBAdapter";
 	private static final String DATABASE_NAME="mycompany.sqlite";
 	private static final int DATABASE_VERSION=1;
-	private static final String TABLE_CREATE = "create table IF NOT EXISTS berkas (_id integer primary key autoincrement, "
-		+ "judul text not null, tag text not null, "
-		+ "path text not null, ext text not null)";
-	private static final String TABLE_DROP = "DROP TABLE IF EXISTS berkas";
-	
+	private static final String KEY_ID = "_id";
+	private static final String TABLE_BERKAS = "berkas";
 	public static final String KEY_ROWID="_id";
 	public static final String KEY_JUDUL="judul";
 	public static final String KEY_TAG="tag";
 	public static final String KEY_PATH="path";
 	public static final String KEY_EXT="ext";
 	private final Context context;
+	private static final String TABLE_CREATE = "create table IF NOT EXISTS berkas (_id integer primary key autoincrement, "
+		+ "judul text not null, tag text not null, "
+		+ "path text not null, ext text not null)";
+	private static final String TABLE_DROP = "DROP TABLE IF EXISTS berkas";
+
 	private DatabaseHelper dbHelper;
 	private SQLiteDatabase db;	
 	
@@ -54,7 +57,10 @@ public class DBAdapter {
 		db = dbHelper.getWritableDatabase();
 		return this;
 	}
-	
+	public DBAdapter opens() throws SQLException {
+		db = dbHelper.getReadableDatabase();
+		return this;
+	}
 	public void close() {
 		dbHelper.close();
 	}
@@ -68,7 +74,39 @@ public class DBAdapter {
 		
 		return db.insert("berkas", null, initialValues);
 	}
-	
+	public void deleteBerkas(String judul){
+		db.delete(TABLE_BERKAS, KEY_JUDUL + " = ?",
+				new String[] { judul });
+	}
+	public void delete(int id){
+		db.delete(TABLE_BERKAS, KEY_ID + " = ?",
+				new String[] { String.valueOf(id) });
+	}
+	public int getID(){
+		int id = 0;
+		return id;
+	}
+	public int getBerkasCount() {
+		String countQuery = "SELECT  * FROM " + TABLE_BERKAS;
+		SQLiteDatabase db=  dbHelper.getReadableDatabase();
+		Cursor cursor = db.rawQuery(countQuery, null);
+
+		int count = cursor.getCount();
+		cursor.close();
+
+		// return count
+		return count;
+	}
+	public int updateBerkas(String judul, String path,String judulAsli) {
+
+		ContentValues values = new ContentValues();
+		values.put(KEY_JUDUL, judul);
+		values.put(KEY_PATH, path);
+
+		// updating row
+		return db.update(TABLE_BERKAS, values, KEY_JUDUL + " = ?",
+				new String[] { judulAsli });
+	}
 	public Cursor getAllBerkas() {
 		return db.query("berkas", new String[] {
 				KEY_ROWID, KEY_JUDUL, KEY_TAG, KEY_PATH, KEY_EXT
