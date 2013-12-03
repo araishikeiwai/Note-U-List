fb26ae818638b6bceb6a9f05894bde431b4617fe
Umar Abdul Aziz <umaraziz.92@gmail.com>
2013-12-04 01:50:12 +0700

    splash+main udah jalan, layout baru :D

--- src/tekmob/nfc/note_u_list/activities/SplashActivity.java
+++ src/tekmob/nfc/note_u_list/activities/SplashActivity.java
@@ -2,26 +2,36 @@ package tekmob.nfc.note_u_list.activities;
 
 import tekmob.nfc.note_u_list.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 
 public class SplashActivity extends Activity {
 
-	//buat halaman pertama yang muncul waktu aplikasi dibuka
-	//ricky tolong bikin asset splashnya juga
-	// buat load data dari database
-	
+	private final int SPLASH_LENGTH = 3000;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
+		getActionBar().hide();
+
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				Intent mainIntent = new Intent(SplashActivity.this,
+						MainActivity.class);
+				SplashActivity.this.startActivity(mainIntent);
+				SplashActivity.this.finish();
+			}
+		}, SPLASH_LENGTH);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.splash, menu);
-		return true;
+	public void onDestroy() {
+		super.onDestroy();
 	}
 
 }
